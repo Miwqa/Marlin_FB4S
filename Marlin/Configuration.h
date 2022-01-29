6e8974c8823685164e7cf945f6e5e8e9f0557b0f
Miwqa <miwqa@list.ru>
2022-01-29 21:54:39 +0300

    my steps and acceleration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -926,7 +926,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 416.7 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 431.1 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -961,9 +961,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          1500    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_ACCELERATION          1000    // X, Y, Z and E acceleration for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  2000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   1000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_TRAVEL_ACCELERATION   2000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk limits (mm/s)
